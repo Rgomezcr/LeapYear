cad40beb6a29c945c90166900c1cd9af558680ea
Raúl Gómez <rgomez@voxelgroup.net>
2021-11-25 17:38:56 +0100

    feat: Be divisible four by four and not be divisible by one hundred

--- LeapYear/LeapYearCalculator.cs
+++ LeapYear/LeapYearCalculator.cs
@@ -8,6 +8,9 @@
                 return false;
             if (number % 400 == 0)
                 return true;
+            if (number % 4 == 0 && number % 100 != 0)
+                return true;
+
             return false;
 
         }
