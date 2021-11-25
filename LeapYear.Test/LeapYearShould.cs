020b9507ac6991f8b59da83301dc3b47f43af2da
Raúl Gómez <rgomez@voxelgroup.net>
2021-11-25 17:33:02 +0100

    feat: Add test and production code

---
+++ LeapYear.Test/LeapYearShould.cs
@@ -0,0 +1,41 @@
+using Xunit;
+
+namespace LeapYear.Test
+{
+    public class LeapYearShould
+    {
+        [Fact]
+        public void BeDivisibleTwoThousandByFourHundred()
+        {
+            bool result = LeapYearCalculator.IsLeapYear(2000);
+            Assert.True(result);
+        }
+        
+        [Fact]
+        public void BeDivisibleFourHundredByFourHundred()
+        {
+            bool result = LeapYearCalculator.IsLeapYear(400);
+            Assert.True(result);
+        }
+        [Fact]
+        public void NotBeDivisibleOneThousandByFourHundred()
+        {
+            bool result = LeapYearCalculator.IsLeapYear(1000);
+            Assert.False(result);
+        }
+        
+        [Fact]
+        public void NotBeDivisibleOneHundredByFourHundredAndBeDivisibleByOneHundred()
+        {
+            bool result = LeapYearCalculator.IsLeapYear(100);
+            Assert.False(result);
+        }
+        
+        [Fact]
+        public void NotBeDivisibleTwoHundredByFourHundredAndBeDivisibleByOneHundred()
+        {
+            bool result = LeapYearCalculator.IsLeapYear(100);
+            Assert.False(result);
+        }
+    }
+}
---
+++ LeapYear/LeapYearCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace LeapYear
+{
+    public class LeapYearCalculator
+    {
+        public static bool IsLeapYear(int number)
+        {
+            if (number % 100 == 0 && number % 400 != 0)
+                return false;
+            if (number % 400 == 0)
+                return true;
+            return false;
+
+        }
+    }
+}
