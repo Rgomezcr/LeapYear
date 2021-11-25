9986b0d540b364ea7afa511d05261226d65075e3
Raúl Gómez <rgomez@voxelgroup.net>
2021-11-25 18:05:12 +0100

    refactor: generalize solution

--- LeapYear/LeapYearCalculator.cs
+++ LeapYear/LeapYearCalculator.cs
@@ -1,12 +1,7 @@
 ﻿namespace LeapYear
 {
-    public class LeapYearCalculator
+    public static class LeapYearCalculator
     {
-        public static bool IsLeapYear(int number)
-        {
-            if (number % 400 == 0)
-                return true;
-            return number % 4 == 0 && number % 100 != 0;
-        }
+        public static bool IsLeapYear(int number) => number % 400 == 0 || number % 4 == 0 && number % 100 != 0;
     }
 }
